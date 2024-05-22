32e8ea0bbed6f0c5f7e28dd37b9e9acbb08bcdcc
Mics <michacaldaira@gmail.com>
2024-05-22 15:49:06 +0800

    chat logs removed scroll

--- app/src/main/java/com/example/taskwiserebirth/SMSFragment.java
+++ app/src/main/java/com/example/taskwiserebirth/SMSFragment.java
@@ -7,7 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -45,20 +44,6 @@ public class SMSFragment extends Fragment {
 
         recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
         recyclerView.setAdapter(chatAdapter);
-
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                if (dy > 0) {
-                    // Scrolling down
-                    ((MainActivity) requireActivity()).toggleNavBarVisibility(false, true);
-                } else if (dy < 0) {
-                    // Scrolling up
-                    ((MainActivity) requireActivity()).toggleNavBarVisibility(true, true);
-                }
-            }
-        });
 
         App app = MongoDbRealmHelper.initializeRealmApp();
         User user = app.currentUser();
